0db9e1e4f42ca1ec9f46e9ed5dae9b24815b1ad2
markarobinson <me@markarobinson.com>
2015-12-19 18:00:01 -0500

    initial commit
    made defualt MVC application

---
+++ smokeStacks/smokeStacks/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="smokeStacks.MvcApplication" Language="C#" %>
---
+++ smokeStacks/smokeStacks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(smokeStacks.Startup))]
+namespace smokeStacks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
